5bcb6373e7237e876f0b1664c259a889280c6bd7
Sergej Ernst <sergej.ernst@outlook.com>
2022-05-21 14:48:59 +0200

    Initial commit

---
+++ src/data/categories.js
@@ -0,0 +1,32 @@
+export const categories = [
+  {
+    id: 1,
+    title: 'Hats',
+    imageUrl:
+      'https://images.unsplash.com/photo-1521369909029-2afed882baee?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
+  },
+  {
+    id: 2,
+    title: 'Jackets',
+    imageUrl:
+      'https://images.unsplash.com/photo-1548950939-629ecb4d7101?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
+  },
+  {
+    id: 3,
+    title: 'Sneakers',
+    imageUrl:
+      'https://images.unsplash.com/photo-1552346154-21d32810aba3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c25lYWtlcnN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500',
+  },
+  {
+    id: 4,
+    title: 'Womens',
+    imageUrl:
+      'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
+  },
+  {
+    id: 5,
+    title: 'Mens',
+    imageUrl:
+      'https://images.unsplash.com/photo-1504593811423-6dd665756598?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
+  },
+];
